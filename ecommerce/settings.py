650455d70128dacdc5d92018ecd127e316cfbfb2
weekend79 <mortenviken@yahoo.com>
2020-05-27 00:28:50 +0000

    Updated ALLOWED_HOST

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost']
+ALLOWED_HOSTS = ['localhost', 'my-first-e-commerce.herokuapp.com']
 
 
 # Application definition
